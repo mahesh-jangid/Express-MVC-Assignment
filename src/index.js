514890ad28cbd3900dfe0ae94f1a7272a3c1a3b6
mahesh-jangid <mesh.jangid@gmail.com>
2022-03-13 22:30:21 +0530

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -4,20 +4,10 @@ const productsController = require("./controllers/attempedevalstudents.controlle
 
 const app = express();
 
-const Users = require("../models/users.model");
 const Students = require("./controllers/attempedevalstudents.controllers");
-const Evaluation = require("../models/evaluation.model");
-const Submission = require("../models/submission.model");
-const Batches = require("../models/batch.model");
-const Results = require("../models/results.model");
 
 app.use(express.json());
 
-app.use("/users", Users);
 app.use("/Students", Students);
-app.use("/Evaluation", Evaluation);
-app.use("/Submission", Submission);
-app.use("/Batches", Batches);
-app.use("/Results", Results);
 
 module.exports = app;
